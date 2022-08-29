7b0125e99fce6095a024dbc09a5ac097d9d17e9b
Svetlana Golovkova <mail.dev.sagolovkova@gmail.com>
2022-08-29 18:54:47 +0300

    signs

---
+++ task_01/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (num == 5 )
+{
+
+}
+else if (num == 6 || num != 3)
+{
+
+}
+else
+{
+  
+}
