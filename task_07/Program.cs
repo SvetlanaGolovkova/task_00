9950206230e093aa35c81fb13713d649411d1130
Svetlana Golovkova <mail.dev.sagolovkova@gmail.com>
2022-08-30 20:19:22 +0300

    minus figure

--- task_07/Program.cs
+++ task_07/Program.cs
@@ -5,9 +5,16 @@
 // 782 -> 2
 // 918 -> 8
 
-Console.WriteLine("Введите целое трехзначное число num: ");
+Console.WriteLine("Введите трехзначное число num: ");
 int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+  Console.WriteLine($"последняя цифра числа {num * -1 % 10}");
+}
+else
+{
+  {Console.WriteLine($"последняя цифра числа {num%10}");}
+}
 
-Console.WriteLine($"последняя цифра числа {num} = {num%10}");
 
 // else if (num1 != square)
